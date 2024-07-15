3bc8669eeb8a921478ba924c5026902a3024879f
Souren Pashangpour <souren@Sourens-MacBook-Pro.local>
2024-07-15 12:35:14 -0400

    works

--- import selenium.py
+++
@@ -1,6 +0,0 @@
-import selenium
-# This is the path I use
-# DRIVER_PATH = '.../Desktop/Scraping/chromedriver 2'
-# Put the path for your ChromeDriver here
-DRIVER_PATH = 'chromedriver_mac64/chromedriver'
-wd = webdriver.Chrome(executable_path=DRIVER_PATH)
